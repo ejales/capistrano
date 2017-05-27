a5453a6aa81c36ef62e2bae8c5237ccf774cb96e
Eric Jales <vagrant@precise32.(none)>
2017-05-27 13:24:34 +0000

    Fix RAND() MySQL

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -3,6 +3,10 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
-    @carousel = Ad.limit(3).order("RANDOM()")
+    if Rails.env.development?
+        @carousel = Ad.limit(3).order("RANDOM()")
+    else
+        @carousel = Ad.limit(3).order("RAND()")
+    end
   end
 end
